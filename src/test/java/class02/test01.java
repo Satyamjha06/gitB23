ea11ede5753d4704b5454e02bc881802ebf03d07
satya <satyamjha06@gmail.com>
2025-05-14 19:58:09 -0400

    2nd commit

--- src/test/java/class02/test01.java
+++ src/test/java/class02/test01.java
@@ -3,5 +3,8 @@ package class02;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello java");
+        System.out.println("hello world");
+        System.out.println("hello 1234");
     }
 }
